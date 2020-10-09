ca1a8dc43c536ea0eb1867548fac6586eabcde39
abkhrr <bukhariadbuk@gmail.com>
2020-10-09 20:36:46 +0700

    open new feature

--- app/src/main/java/com/foodemi/foodemimenu/ui/view/feature/activity/MainActivity.kt
+++ app/src/main/java/com/foodemi/foodemimenu/ui/view/feature/activity/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.foodemi.foodemimenu.ui.view.feature.activity
 
+import android.animation.AnimatorInflater
+import android.animation.StateListAnimator
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.findNavController
 import com.foodemi.foodemimenu.R
 import com.foodemi.foodemimenu.BR
 import com.foodemi.foodemimenu.databinding.ActivityMainBinding
@@ -11,6 +15,7 @@ import com.foodemi.foodemimenu.ui.view.base.CoreActivity
 import com.foodemi.foodemimenu.ui.viewmodel.factory.ViewModelFactory
 import com.foodemi.foodemimenu.ui.viewmodel.feature.activity.ActivityMainViewModel
 import dagger.android.HasAndroidInjector
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class MainActivity : CoreActivity<ActivityMainBinding, ActivityMainViewModel>(),
@@ -18,8 +23,6 @@ class MainActivity : CoreActivity<ActivityMainBinding, ActivityMainViewModel>(),
 
     @Inject
     lateinit var factory: ViewModelFactory
-
-    private lateinit var appBarConfig: AppBarConfiguration
 
     override val bindingVariable: Int
         get() = BR.viewModel
@@ -33,12 +36,62 @@ class MainActivity : CoreActivity<ActivityMainBinding, ActivityMainViewModel>(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupOrientation()
+        setupDestinationChanged()
+    }
+
+    private fun setupDestinationChanged(){
+        findNavController(R.id.nav_host_fragment).addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.pickTable        -> {
+                    hideToolbar()
+                    appBarLayoutMain.stateListAnimator = backToElevation()
+                }
+                R.id.menuList        -> {
+                    showToolbar()
+                    hideToolbarBackButton()
+                    appBarLayoutMain.stateListAnimator = backToElevation()
+                }
+                R.id.menuDetails        -> {
+                    showToolbar()
+                    actionBarBackButton()
+                    appBarLayoutMain.stateListAnimator = backToElevation()
+                }
+            }
+        }
     }
 
     private fun setupOrientation(){
         if(resources.getBoolean(R.bool.portrait_only)){
             requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         }
+    }
+
+    private fun actionBarBackButton(): ActionBar?{
+        return supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    private fun hideToolbarBackButton(): ActionBar?{
+        return supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(false)
+            setHomeButtonEnabled(false)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    private fun showToolbar(){
+        view_toolbar_main.visibility = View.VISIBLE
+    }
+
+    private fun hideToolbar(){
+        view_toolbar_main.visibility = View.INVISIBLE
+    }
+
+    private fun backToElevation(): StateListAnimator {
+        return AnimatorInflater.loadStateListAnimator(this, R.animator.appbar_always_elevated)
     }
 
 }
